8f5c340a2b3cd80d7677d02d4d1e0002036256b8
tanakanata <ta1na6ka16@gmail.com>
2021-09-18 23:33:09 +0900

    Initial commit

---
+++ src/main/java/com/github/tanakanata/disableenderdragon/DisableEnderdragon.java
@@ -0,0 +1,49 @@
+package com.github.tanakanata.disableenderdragon;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DisableEnderdragon extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        this.getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("有効化しました。");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onPlayerInteractEvent(PlayerInteractEvent event){
+        //ブロック設置のイベントか判定
+        if (!event.hasBlock()){
+            return;
+        }
+        //イベントが発生したディメンションがエンドか判定
+        if (event.getPlayer().getWorld().getEnvironment() != World.Environment.THE_END){
+            return;
+        }
+        //使用したアイテムがエンドクリスタルか判定
+        if (event.getMaterial() != Material.END_CRYSTAL ) {
+            return;
+        }
+        //クリックしたブロックが岩盤かどうか判定
+        if (event.getClickedBlock().getType() != Material.BEDROCK){
+            return;
+        }
+        //設置イベントをキャンセル
+        event.setCancelled(true);
+        event.getPlayer().sendMessage("§Cこのワールドでは再召喚できません。");
+    }
+
+}
